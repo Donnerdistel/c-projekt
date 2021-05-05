fcc736aeb88f6886c56555eec68dd997caf2c3f6
Donnerdistel <hartmannjs@icloud.com>
2021-05-05 14:07:09 +0200

    Final 2

--- main.c
+++ main.c
@@ -11,11 +11,15 @@
 struct node {
     char		*w;
     int	 	count;
+    char* line;
     struct node	*l, *r;
 } *tree;
 
+//char* lineTEMP = NULL;
+
 //Binärbaum aufbauen
 void incr(struct node **root, char *w) {
+
     if (root == NULL || w == NULL || *w == '\0')
         return;
     if (*root == NULL) {
@@ -32,6 +36,7 @@ void incr(struct node **root, char *w) {
         n->count = 1;
         n->l = NULL;
         n->r = NULL;
+        //n->line = lineTEMP;
 
         *root = n;
         return;
@@ -53,7 +58,9 @@ void traverse_search(struct node *r) {
         return;
     traverse_search(r->l);		//laufe linken Teilbaum durch
     if((*r).count == 1){
-        printf("%s %d\n",r->w, r->count);
+        //printf("%s %d \t\t\t%s\n",r->w, r->count, r->line);
+        //printf("%s %d \n",r->w, r->count);
+        printf("%s \n",r->w);
     };
     traverse_search(r->r);				//laufe rechten Teilbaum durch
 }
@@ -65,7 +72,7 @@ void words(char *s, char *end) {
 
     while (s <= end) {
         c = *s;
-        if (isalpha(c)) {
+        if (!(c == '-') && !(c == '(') && !(c==')') && !(c=='\"') && !isblank(c) && !isdigit(c) && !(c == '<')) {
             if (!state) {
                 start = s;
                 state = 1;
@@ -103,14 +110,30 @@ char *read_file(char *file_name){
     char *end = NULL;
     while (getline(&line, &len, fp) != -1) {
         //Suche <author und <editor in der Zeile
-        if (strstr(line, "<author") != NULL || strstr(line, "<editor") != NULL){
-            start = strchr(line, '>');       //erstes Vorkommen von >
-            end = strrchr(line, '<');        //letztes Vorkommen von <
+        if (strstr(line, "<author") != NULL)
+        {
+            start = strstr(line, "<author");
+            start = strchr(start, '>');               //erstes Vorkommen von >
+            end = strstr(line, "</author");        //letztes Vorkommen von </author
 
             //konvertiere das Intervall in Wörter
             start++;      //Zeichen hinter >
+            //lineTEMP = line;
             words(start, end);
         }
+        else if (strstr(line, "<editor") != NULL) {
+            start = strstr(line, "<editor");
+            start = strchr(line, '>');              //erstes Vorkommen von >
+            end = strstr(line, "</editor");      //letztes Vorkommen von </editor
+
+            //konvertiere das Intervall in Wörter
+            start++;      //Zeichen hinter >
+            //lineTEMP = line;
+            words(start, end);
+        }
+
+        //printf("Start: %s und Ende: %s\n", start, end);
+        //lineTEMP = NULL;
         line = NULL;
     }
 }
